89368b119341a4ee56d96a931213f625bda84219
Jickx <63093696+Jickx@users.noreply.github.com>
2022-08-05 13:03:35 +0300

    delete folder

--- python-bascics/perfect.py
+++
@@ -1,7 +0,0 @@
-def is_perfect(number):
-    number_list = [i for i in range(1, number) if number % i == 0]
-    return number_list or False
-
-
-assert is_perfect(6) == [1, 2, 3]
-assert is_perfect(28) == [1, 2, 4, 7, 14]
--- python-bascics/power_of_three.py
+++
@@ -1,7 +0,0 @@
-def is_power_of_three(x):
-    while x % 3 == 0:
-        x /= 3
-    return x == 1
-
-print(is_power_of_three(9))
-    
