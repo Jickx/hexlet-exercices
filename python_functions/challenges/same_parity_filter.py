ae6c19f56447704f9e40618c3edd5124b429da0c
Jickx <63093696+Jickx@users.noreply.github.com>
2022-09-07 14:37:06 +0300

    refactor

--- python_functions/challenges/same_parity_filter.py
+++ python_functions/challenges/same_parity_filter.py
@@ -2,20 +2,21 @@
 # возвращает новый список, состоящий из элементов, у которых такая же
 # чётность, как и у первого элемента исходного списка.
 
+def check_parity(num):
+    return num % 2 == 0
+
 
 def same_parity_filter(nums: list[int]) -> list:
-    if nums:
-        if nums[0] % 2 == 0:
-            return list(filter(lambda x: x % 2 == 0, nums))
-        else:
-            return list(filter(lambda x: x % 2 != 0, nums))
-    else:
+    if not nums:
         return []
 
+    parity_first_number = nums[0] % 2 == 0
 
-print(same_parity_filter([]))
-# []
-print(same_parity_filter([2, 0, 1, -3, 10, -2]))
-# [2, 0, 10, -2]
-print(same_parity_filter([-1, 0, 1, -3, 10, -2]))
-# [-1, 1, -3]
+    return list(filter(
+        lambda x: parity_first_number == check_parity(x), nums
+    ))
+
+
+assert same_parity_filter([]) == []
+assert same_parity_filter([2, 0, 1, -3, 10, -2]) == [2, 0, 10, -2]
+assert same_parity_filter([-1, 0, 1, -3, 10, -2]) == [-1, 1, -3]
