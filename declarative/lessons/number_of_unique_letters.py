c0826801c6b3f07361c47734f80a90480741d346
Jickx <63093696+Jickx@users.noreply.github.com>
2022-09-14 13:46:50 +0300

    add unique letters

---
+++ declarative/lessons/number_of_unique_letters.py
@@ -0,0 +1,12 @@
+# Вам необходимо реализовать функцию number_of_unique_letters(), которая должна
+# подсчитывать количество уникальных букв в строке-аргументе без учёта регистра
+
+def number_of_unique_letters(some_string: str) -> int:
+    return len(set([i.lower() for i in some_string if i.isalpha()]))
+
+
+assert number_of_unique_letters("") == 0
+assert number_of_unique_letters("abc") == 3
+assert number_of_unique_letters("A-a-a-a-a-a!") == 1
+assert number_of_unique_letters("\\(O_o)/") == 1
+assert number_of_unique_letters("AaBbCcDd") == 4
