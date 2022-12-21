dd1dd23244ebfd87edb4ca5c0e3c7d8498ffbd59
Jickx <63093696+Jickx@users.noreply.github.com>
2022-12-21 16:27:07 +0200

    refactor

--- python_trees/lessons/compress_images.py
+++ python_trees/lessons/compress_images.py
@@ -9,13 +9,20 @@ from hexlet.fs import get_children, get_meta, get_name, is_file, mkdir, mkfile
 
 
 def compress_images(tree):
-    tree_copy = copy.deepcopy(tree)
-    childrens = get_children(tree_copy)
-    for children in childrens:
-        if get_name(children)[-4:] == '.jpg' and \
-                children['meta'].get('size', None):
-            children['meta']['size'] /= 2
-    return tree_copy
+    children = get_children(tree)
+
+    def reduce_image_size(node):
+        name = get_name(node)
+        if not is_file(node) or not name.endswith('.jpg'):
+            return node
+        meta = get_meta(node)
+        new_meta = copy.deepcopy(meta)
+        new_meta['size'] //= 2
+        return mkfile(name, new_meta)
+
+    new_children = map(reduce_image_size, children)
+    new_meta = copy.deepcopy(get_meta(tree))
+    return mkdir(get_name(tree), list(new_children), new_meta)
 
 
 tree = mkdir(
@@ -23,7 +30,6 @@ tree = mkdir(
     [
         mkfile('avatar.jpg', {'size': 100}),
         mkfile('photo.jpg', {'size': 150}),
-        mkfile('photo.jpg')
     ],
     {'hide': False}
 )
