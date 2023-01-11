5b7f5ffa25e709ebcf4b5393814e5e9df8114003
Jickx <63093696+Jickx@users.noreply.github.com>
2023-01-11 20:53:43 +0200

    refactor

--- python_trees/lessons/get_hidden_files_count.py
+++ python_trees/lessons/get_hidden_files_count.py
@@ -1,17 +1,33 @@
-items = [2, [3, [4, [5, [6]]], [7]]]
+# Реализуйте функцию get_hidden_files_count(), которая считает количество
+# скрытых файлов в директории и всех поддиректориях. Скрытым файлом в Linux
+# системах считается файл, название которого начинается с точки:
+
+from hexlet.fs import mkdir, mkfile, is_file, get_name, get_children
 
 
-def check(num):
-    if num % 2 == 0 and num > 4:
-        return num
-    return 1
+def get_hidden_files_count(node):
+    name = get_name(node)
+    if is_file(node) and name.startswith('.'):
+        return 1
+    childrens = get_children(node)
+    count = list(map(get_hidden_files_count, childrens))
+    return sum(count)
 
 
-def agregate(items):
-    if isinstance(items, int):
-        return check(items)
-    result = list(map(agregate, items))
-    return sum(result)
 
-
-print(agregate(items))
+tree = mkdir('/', [
+    mkdir('etc', [
+        mkdir('apache'),
+        mkdir('nginx', [
+            mkfile('.nginx.conf', {'size': 800}),
+        ]),
+        mkdir('.consul', [
+            mkfile('.config.json', {'size': 1200}),
+            mkfile('data', {'size': 8200}),
+            mkfile('raft', {'size': 80}),
+        ]),
+    ]),
+    mkfile('.hosts', {'size': 3500}),
+    mkfile('resolve', {'size': 1000}),
+])
+print(get_hidden_files_count(tree))  # 3
