67327f06fbdb5da6467f398424372d8a9b0a5d66
Jickx <63093696+Jickx@users.noreply.github.com>
2023-01-11 11:44:54 +0200

    refactor

--- python_trees/lessons/compress_images.py
+++ python_trees/lessons/compress_images.py
@@ -4,42 +4,46 @@
 # обновленную директорию со сжатыми картинками и всеми остальными данными,
 # которые были внутри этой директории.
 
+from hexlet.fs import mkdir, mkfile, get_children, get_meta, get_name
 import copy
-from hexlet.fs import get_children, get_meta, get_name, is_file, mkdir, mkfile
+from pprint import pprint
 
 
 def compress_images(tree):
-    children = get_children(tree)
+    new_tree = copy.deepcopy(tree)
+    new_childrens = get_children(new_tree)
 
-    def reduce_image_size(node):
-        name = get_name(node)
-        if not is_file(node) or not name.endswith('.jpg'):
-            return node
+    def change_size(node):
         meta = get_meta(node)
-        new_meta = copy.deepcopy(meta)
-        new_meta['size'] //= 2
-        return mkfile(name, new_meta)
+        size = meta.get('size')
+        if get_name(node).endswith('.jpg') and size:
+            meta['size'] //= 2
+            return mkfile(get_name(node), meta)
+        else:
+            return node
 
-    new_children = map(reduce_image_size, children)
-    new_meta = copy.deepcopy(get_meta(tree))
-    return mkdir(get_name(tree), list(new_children), new_meta)
+    childrens_list = list(map(lambda x: change_size(x), new_childrens))
+    return mkdir(get_name(new_tree), childrens_list, get_meta(new_tree))
 
 
-tree = mkdir(
-    'my documents',
-    [
-        mkfile('avatar.jpg', {'size': 100}),
-        mkfile('photo.jpg', {'size': 150}),
-    ],
-    {'hide': False}
-)
-print(compress_images(tree))
-# {
-#     'name': 'my documents',
-#     'type': 'directory',
-#     'children': [
-#         {'name': 'avatar.jpg', 'meta': {'size': 50}, 'type': 'file'},
-#         {'name': 'photo.jpg', 'meta': {'size': 75}, 'type': 'file'},
-#     ],
-#     'meta': {'hide': False},
-# }
+tree = mkdir('my documents', [
+            mkfile(
+                'avatar.jpg',
+                {'size': 100, 'attributes': {'hide': False, 'read_only': True}},
+            ),
+            mkdir('presentations'),
+        ], {'owner': 'hexlet'})
+
+new_tree = compress_images(tree)
+new_file = get_children(new_tree)[0]
+new_file_meta = get_meta(new_file)
+new_file_meta['attributes']['hide'] = True
+old_file = get_children(tree)[0]
+assert not get_meta(old_file)['attributes']['hide']
+
+new_tree_meta = get_meta(new_tree)
+new_tree_meta['owner'] = 'user'
+
+pprint(compress_images(tree) == tree)
+pprint(tree)
+
