5e3a44645969eece6acfd7e8398fab26fb35045a
Jickx <63093696+Jickx@users.noreply.github.com>
2023-02-07 18:18:04 +0200

    add convert

---
+++ python_trees/challenges/convert.py
@@ -0,0 +1,42 @@
+# Реализуйте функцию convert(), которая принимает на вход список определённой
+# структуры и возвращает словарь, полученный из этого списка.
+
+# Список устроен таким образом, что с помощью него можно представлять словари.
+# Каждый элемент списка — тоже список из двух элементов, где первый элемент —
+# ключ, а второй — значение. Значение тоже может быть списком. Любой список
+# внутри исходного списка всегда рассматривается как данные, которые нужно
+# конвертировать в словарь.
+
+
+def convert(tree):
+    def to_item(item):
+        key, value = item
+        return key, convert(value) if isinstance(value, list) else value
+
+    return dict(map(to_item, tree))
+
+
+tree = [
+    ['key4', 'value4'],
+    ['anotherKey', [
+        ['key7', False],
+        ['innerKey', []],
+    ],
+     ],
+    ['key6', None],
+    ['anotherKey2', [
+        ['wow', [['one', 'two'], ['three', 'four']]],
+        ['key5', True],
+    ],
+     ],
+]
+
+expected = {
+    'key4': 'value4',
+    'anotherKey': {'key7': False, 'innerKey': {}},
+    'key6': None,
+    'anotherKey2': {'wow': {'one': 'two', 'three': 'four'}, 'key5': True},
+}
+
+
+assert convert(tree) == expected
