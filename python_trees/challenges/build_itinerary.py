04db2077837338d2670c7ce2a28171b6b3b1e231
Jickx <63093696+Jickx@users.noreply.github.com>
2023-02-09 22:59:30 +0200

    add build itianary, still not working

--- python_trees/challenges/build_itinerary.py
+++ python_trees/challenges/build_itinerary.py
@@ -5,32 +5,32 @@
 from copy import deepcopy
 
 
-def build_itinerary(tree, start, finish):
+def build_itinerary(tree, start_point, finish_point):
     result = []
-    parent_city_list = []
+    parent_dict = {}
+
+    def get_route_from_parent_dict(parent_dict):
+        pass
 
 
-    def walk(subtree, start):
-        found = True
+    def walk(subtree, point, parent=None):
         if len(subtree) == 2 and not isinstance(subtree[0], list):
             [city, route] = subtree
-            parent_city_list.append(city)
-            return walk(route, start)
-        if found is not True:
-            parent_city_list.pop()
-            found = False
-        for city in subtree:
-            if len(city) != 1:
-                walk(city, start)
-            elif city == [start]:
-                found = True
-                result.append(deepcopy(parent_city_list))
-                result.append(city)
-        map(walk, subtree)
-        print(f'Parent city list: {parent_city_list}')
-        return parent_city_list
-    walk(tree, start)
-    return result
+            parent_dict[city] = parent
+            parent = city
+            return walk(route, point, parent)
+        for item in subtree:
+            if len(item) != 1:
+                walk(item, point, parent)
+            elif item == [point]:
+                result.append(parent_dict)
+                result.append(item)
+        return result
+    start_path = deepcopy(walk(tree, start_point))
+    parent_dict.clear()
+    result.clear()
+    finish_path = deepcopy(walk(tree, finish_point))
+    return start_path, finish_path
 
 
 
@@ -59,5 +59,5 @@ tree = ['Moscow', [
 print(build_itinerary(tree, 'Dubna', 'Kostroma'))
 # ['Dubna', 'Tver', 'Moscow', 'Ivanovo', 'Kostroma']
 
-build_itinerary(tree, 'Borisovka', 'Kurchatov')
+# build_itinerary(tree, 'Borisovka', 'Kurchatov')
 # ['Borisovka', 'Belgorod', 'Kursk', 'Kurchatov']
