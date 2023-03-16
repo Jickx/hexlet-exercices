8c4f165c4f617b9bd733040d2ce92a60a8315b94
Jickx <63093696+Jickx@users.noreply.github.com>
2023-03-16 21:48:03 +0200

    refactor

--- python_trees/challenges/build_itinerary.py
+++ python_trees/challenges/build_itinerary.py
@@ -3,50 +3,62 @@
 # в котором они находятся на пути следования по маршруту.
 
 
-def build_itinerary(tree, start_point, finish_point):
-
-    def generate_parent_dict(subtree, parent=None, parent_dict={}):
-        if len(subtree) == 2 and not isinstance(subtree[0], list):
-            [city, path] = subtree
+def generate_parent_dict(subtree, parent=None, parent_dict={}):
+    if len(subtree) == 2 and not isinstance(subtree[0], list):
+        [city, path] = subtree
+        parent_dict[city] = parent
+        parent = city
+        return generate_parent_dict(path, parent)
+    for item in subtree:
+        if len(item) == 1:
+            [city] = item
             parent_dict[city] = parent
-            parent = city
-            return generate_parent_dict(path, parent)
-        for item in subtree:
-            if len(item) == 1:
-                [city] = item
-                parent_dict[city] = parent
-            else:
-                generate_parent_dict(item, parent)
-        return parent_dict
+        else:
+            generate_parent_dict(item, parent)
+    return parent_dict
 
-    def generate_path_from_parent_dict(parent_dict, pathpoint):
-        result = []
 
-        def walk(parent_dict, curr_point=pathpoint):
-            if not parent_dict[curr_point]:
-                result.append(curr_point)
-                return parent_dict[curr_point]
+def generate_path_from_parent_dict(parent_dict, pathpoint):
+    result = []
+
+    def walk(parent_dict, curr_point=pathpoint):
+        if not parent_dict[curr_point]:
             result.append(curr_point)
-            curr_point = parent_dict[curr_point]
-            return walk(parent_dict, curr_point)
-        walk(parent_dict)
-        return result
+            return parent_dict[curr_point]
+        result.append(curr_point)
+        curr_point = parent_dict[curr_point]
+        return walk(parent_dict, curr_point)
 
-    def calculate_start_to_finish(start_point_path, finish_point_path):
-        finish_point_path.reverse()
-        for point in start_point_path:
-            if point in finish_point_path:
-                common_point = point
-                break
-        x = start_point_path[0:start_point_path.index(common_point)]
-        y = finish_point_path[finish_point_path.index(common_point):]
-        return x + y
+    walk(parent_dict)
+    return result
 
+
+def calculate_start_to_finish(start_point_path, finish_point_path):
+    finish_point_path.reverse()
+    for point in start_point_path:
+        if point in finish_point_path:
+            common_point = point
+            break
+    x = start_point_path[0:start_point_path.index(common_point)]
+    y = finish_point_path[finish_point_path.index(common_point):]
+    return x + y
+
+
+def build_itinerary(tree, start_point, finish_point):
     parent_dict = generate_parent_dict(tree)
-    start_point_path = generate_path_from_parent_dict(parent_dict, start_point)
-    finish_point_path = generate_path_from_parent_dict(parent_dict, finish_point)
+    start_point_path = generate_path_from_parent_dict(
+        parent_dict,
+        start_point
+    )
+    finish_point_path = generate_path_from_parent_dict(
+        parent_dict,
+        finish_point
+    )
 
-    return calculate_start_to_finish(start_point_path, finish_point_path)
+    return calculate_start_to_finish(
+        start_point_path,
+        finish_point_path
+    )
 
 
 tree = ['Moscow', [
