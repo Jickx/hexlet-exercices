c944c8fbcc482f96b2cc159efcf2e1ef4146963f
jickx <valentin.sharonov@gmail.com>
2023-07-20 12:04:41 +0300

    add prettify_html_file.py.py

---
+++ python-advanced-testing/lessons/prettify_html_file.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+
+
+def read(file_path):
+    with open(file_path, 'r') as f:
+        result = f.read()
+    return result
+
+
+def write(file_path, data):
+    with open(file_path, 'w') as f:
+        f.write(data)
+
+
+def prettify(file_path):
+    data = read(file_path)
+
+    write(
+        file_path,
+        BeautifulSoup(data, 'html.parser').prettify()
+    )
+
